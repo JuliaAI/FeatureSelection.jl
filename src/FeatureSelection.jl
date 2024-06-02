1372315de6a2c11df60a28f3a4d5355f6ee13f9d
OkonSamuel <okonsamuel50@gmail.com>
2024-06-02 20:03:57 +0100

    fix docstring and build documentation

--- src/FeatureSelection.jl
+++ src/FeatureSelection.jl
@@ -10,18 +10,4 @@ const MMI = MLJModelInterface
 include("models/featureselector.jl")
 include("models/rfe.jl")
 
-## Pkg Traits
-MMI.metadata_pkg.(
-    (
-        DeterministicRecursiveFeatureElimination,
-        ProbabilisticRecursiveFeatureElimination, 
-        FeatureSelector
-    ),
-    package_name       = "FeatureSelection",
-    package_uuid       = "33837fe5-dbff-4c9e-8c2f-c5612fe2b8b6",
-    package_url        = "https://github.com/JuliaAI/FeatureSelection.jl",
-    is_pure_julia      = true,
-    package_license    = "MIT"
-)
-
 end # module
